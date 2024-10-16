9e34ba39958323783dc24a0649a5be551337a746
segoshi <chokokomint@icloud.com>
2024-10-16 19:18:38 +0900

    gitignore modified

--- create_users.py
+++ create_users.py
@@ -25,4 +25,24 @@ def create_users(n):
 
     CustomUser.objects.bulk_create(users, ignore_conflicts=True)
 
+    my_id = CustomUser.objects.get(username="ニューアドミン").id
+
+    # values_list メソッドを使うと、User オブジェクトから特定のフィールドのみ取り出すことができます。
+    # 返り値はユーザー id のリストになります。
+    user_ids = CustomUser.objects.exclude(id=my_id).values_list("id", flat=True)
+
+    talks = []
+    for _ in range(len(user_ids)):
+        sent_talk = Talk(
+            sender_id=my_id,
+            receiver_id = random.choice(user_ids),
+            message = fakegen.text(),
+        )
+        received_talk = Talk(
+            sender_id = random.choice(user_ids),
+            receiver_id = my_id,
+            message = fakegen.text()
+        )
+        talks.extend([sent])
+
     
